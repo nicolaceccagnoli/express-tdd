a08d1e9528dadddfb740eccbb49ce0191d0d4a51
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-05-30 15:23:01 +0200

    reached point 4

--- test/slug.test.js
+++ test/slug.test.js
@@ -1,10 +1,34 @@
 // Importo Jest
 const { test, expect } = require("@jest/globals");
 
+// Definisco un array per gli slugs
+let slugs = [];
+
 const createSlug = (str) => {
 
-    return str.toLowerCase().replaceAll(' ', '-');
+    const slugBase = str.toLowerCase().replaceAll(' ', '-');
+
+    let counter = 1;
+
+    let slug = slugBase;
+
+    while(slugs.includes(slug)) {
+        slug = `${slugBase}-${counter}`;
+
+        counter++;
+    }
+
+    slugs.push(slug);
+
+    return slug;
+
 } 
+
+// Resetto l'array slugs prima di ogni test
+beforeEach(() => {
+    slugs = [];
+});
+
 
 // createSlug dovrebbe ritornare una stringa
 test('createSlug dovrebbe ritornare una stringa', () => {
@@ -33,5 +57,9 @@ test('createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -', 
 
 // createSlug dovrebbe incrementare di 1 lo slug quando esiste già
 test('createSlug dovrebbe incrementare di 1 lo slug quando esiste già', () => {
-    
+    const slug1 = createSlug('Hello World');
+
+    const slug2 = createSlug('Hello World');
+
+    expect(slug2).toBe('hello-world-1');
 })
